1207edc62057525c7db18f46e74a6d9ddc0e92c8
manycoding <manycoding@users.noreply.github.com>
2020-06-26 13:08:35 -0400

    Add zappa

--- api.py
+++ api.py
@@ -12,6 +12,8 @@ ALGOLIA_ID = os.environ.get("ALGOLIA_ID")
 ALGOLIA_KEY = os.environ.get("ALGOLIA_KEY")
 ALGOLIA_INDEX = os.environ.get("ALGOLIA_INDEX")
 DEBUG = os.environ.get("DEBUG", False)
+client = SearchClient.create(ALGOLIA_ID, ALGOLIA_KEY)
+index = client.init_index(ALGOLIA_INDEX)
 
 
 class Similar(Resource):
@@ -36,6 +38,4 @@ class Similar(Resource):
 api.add_resource(Similar, "/similar/<string:object_id>")
 
 if __name__ == "__main__":
-    client = SearchClient.create(ALGOLIA_ID, ALGOLIA_KEY)
-    index = client.init_index(ALGOLIA_INDEX)
     app.run(debug=DEBUG)
